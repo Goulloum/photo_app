f54b089677b49c9dcf077d4425931f8afa7c9cc0
Mathieu GUILLEMIN <mathieu.guillemin@sfl.com>
2023-12-11 09:57:52 +0100

    forogot to push uwu --><--

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,13 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
-    {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
-    }
+
 
     #[Route('/admin/gallery', name: 'app_admin_gallery')]
     public function gallery(GalleryRepository $galleryRepository): Response
