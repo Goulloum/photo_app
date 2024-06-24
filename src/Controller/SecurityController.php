75408baffa58d977bb6dc6f9280df9bb721774b2
Goulloum <math.guillemin@hotmail.fr>
2024-06-24 10:35:12 +0200

    chore: Remove commented out code in SecurityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,10 +12,6 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
@@ -29,6 +25,4 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-
-    
 }
